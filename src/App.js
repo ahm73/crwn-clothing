af5f12364a1cd20506a0a55a5bd35466f21e3981
arquitectonet2 <arquitectonet2@funcionpublica.gob.mx>
2021-12-17 01:11:00 -0600

    updatting our app witha shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,46 +4,47 @@ import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import  HomePage  from './components/pages/homepage/homepage.component';
+import ShopPage from './components/pages/shop/shop.component';
 
-const HatsPage = () => {
-  console.log("Hats page... function good")
-  return(
-    <div>
-      <h1>HATS PAGES</h1>
-    </div>
-  )
-  };
+// const ShopPage = () => {
+//   console.log("Shop page... function good")
+//   return(
+//     <div>
+//       <h1>SHOP PAGES</h1>
+//     </div>
+//   )
+//   };
 
-const HomePage1 =(props) => {  
-  console.log(props)
-return (
-    <div>
-      <button onClick={() => props.history.push('/topics')} >Topics</button>
-      <h1>HOME PAGE</h1>
-    </div>
-  )
-}
+// const HomePage1 =(props) => {  
+//   console.log(props)
+// return (
+//     <div>
+//       <button onClick={() => props.history.push('/topics')} >Topics</button>
+//       <h1>HOME PAGE</h1>
+//     </div>
+//   )
+// }
 
-const TopicList =(props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1> TOPIC LIST PAGE</h1>
-      <Link to= {`${props.match.url}/13`}> To Topic 13</Link>
-      <Link to= {`${props.match.url}/17`}> To Topic 17</Link>
-      <Link to= {`${props.match.url}/21`}> To Topic 21</Link>
-    </div>
-  )
-  }
+// const TopicList =(props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <h1> TOPIC LIST PAGE</h1>
+//       <Link to= {`${props.match.url}/13`}> To Topic 13</Link>
+//       <Link to= {`${props.match.url}/17`}> To Topic 17</Link>
+//       <Link to= {`${props.match.url}/21`}> To Topic 21</Link>
+//     </div>
+//   )
+//   }
 
-const TopicDetail = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE: { props.match.params.topicId} </h1>
-    </div>
-  );
- };
+// const TopicDetail = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>TOPIC DETAIL PAGE: { props.match.params.topicId} </h1>
+//     </div>
+//   );
+//  };
 
 
 function App() {
@@ -52,7 +53,7 @@ function App() {
     <div >
       <Switch>
         <Route  exact path='/' component={HomePage} />
-        <Route  path='/hats' component= { HatsPage } />
+        <Route  path='/shop' component= { ShopPage } />
         {/* <Route  exact path='/topics' component={TopicList} />
         <Route  path='/topics/:topicId' component={TopicDetail} /> */}
       </Switch>
---
+++ src/components/preview-collection.component/preview-collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './preview-collection.styles.scss'
+
+const PreviewCollection = () => {
+    return (
+        <div>
+            PreviewCollection
+        </div>
+    )
+}
+
+export default PreviewCollection
